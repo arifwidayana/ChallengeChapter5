96dbc15528c1bafe2ee9f79bebe359f5de155789
Arif Widayana <arifwidayana@gmail.com>
2022-04-22 06:38:32 -0700

    update API + Fix networking id

--- app/src/main/java/com/arifwidayana/challengechapter5/model/service/ApiClient.kt
+++ app/src/main/java/com/arifwidayana/challengechapter5/model/service/ApiClient.kt
@@ -1,12 +1,12 @@
 package com.arifwidayana.challengechapter5.model.service
 
+import com.arifwidayana.challengechapter5.model.utils.Constant
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "https://rent-cars-api.herokuapp.com/"
 
     private val log: HttpLoggingInterceptor
         get() {
@@ -18,11 +18,19 @@ object ApiClient {
 
     private val client = OkHttpClient.Builder()
         .addInterceptor(log)
+        .addInterceptor{
+            val req = it.request()
+            val query = req.url
+                .newBuilder()
+                .addQueryParameter(Constant.API_NAME, Constant.API_KEY)
+                .build()
+            return@addInterceptor it.proceed(req.newBuilder().url(query).build())
+        }
         .build()
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
+            .baseUrl(Constant.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
--- app/src/main/java/com/arifwidayana/challengechapter5/model/service/ApiService.kt
+++ app/src/main/java/com/arifwidayana/challengechapter5/model/service/ApiService.kt
@@ -1,10 +1,29 @@
 package com.arifwidayana.challengechapter5.model.service
 
+import com.arifwidayana.challengechapter5.model.pojo.details.DetailsMovie
+import com.arifwidayana.challengechapter5.model.pojo.nowplaying.NowPlayingMovie
+import com.arifwidayana.challengechapter5.model.pojo.popular.PopularMovie
+import com.arifwidayana.challengechapter5.model.pojo.toprated.TopRatedMovie
+import com.arifwidayana.challengechapter5.model.pojo.upcoming.UpComingMovie
+import com.arifwidayana.challengechapter5.model.utils.Constant
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
-    //@GET("admin/car")
-    //fun getCars(): Call<List<GetCarsResponseItem>>
+    @GET(Constant.NOW_PLAYING)
+    fun getNowPlayingMovies(): Call<NowPlayingMovie>
+
+    @GET(Constant.POPULAR)
+    fun getPopularMovies(): Call<PopularMovie>
+
+    @GET(Constant.UP_COMING)
+    fun getUpComingMovies(): Call<UpComingMovie>
+
+    @GET(Constant.TOP_RATED)
+    fun getTopRatedMovies(): Call<TopRatedMovie>
+
+    @GET(Constant.DETAIL_MOVIES)
+    fun getDetailMovies(@Path("id") id: Int?): Call<DetailsMovie>
 }
